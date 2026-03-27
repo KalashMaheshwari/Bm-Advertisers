9683055fa8e9d5620a82fecd03bc71eafeeedbbf
KalashMaheshwari <kalashviyani0108@gmail.com>
2026-03-28 00:51:49 +0530

    still...

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-export default defineConfig(async () => {
-  const plugins = [react(), tailwindcss()];
-  try {
-    // @ts-ignore
-    const m = await import('./.vite-source-tags.js');
-    plugins.push(m.sourceTags());
-  } catch { }
-  return {
-    base: '/Bm-Advertisers/',
-    plugins
-  };
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    // THIS COPIES THE WORKER SO GITHUB PAGES CAN FIND IT LOCALLY
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
+          dest: './'
+        }
+      ]
+    })
+  ],
+  base: '/Bm-Advertisers/', 
 })
