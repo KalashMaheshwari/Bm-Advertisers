908cecf0d1ce60ad68ef8b939bdf4924abac12ff
KalashMaheshwari <kalashviyani0108@gmail.com>
2026-03-28 01:10:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // THIS COPIES THE WORKER SO GITHUB PAGES CAN FIND IT LOCALLY
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-          dest: './'
-        }
-      ]
-    })
   ],
   base: '/Bm-Advertisers/', 
 })
